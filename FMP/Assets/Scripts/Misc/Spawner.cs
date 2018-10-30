a5baa6d051901a3b0a773c9ea34168084ec02c97
Widdiful <Widdiful@users.noreply.github.com>
2018-10-30 03:07:52 +0000

    Water stuff

---
+++ FMP/Assets/Scripts/Misc/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject objectToSpawn;
+    public float spawnRate;
+    public int max;
+    private float spawnTimer;
+    private int spawned;
+
+    void Update() {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0 && (spawned < max || max <= 0)) {
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            spawnTimer = spawnRate;
+            spawned++;
+        }
+    }
+
+}
