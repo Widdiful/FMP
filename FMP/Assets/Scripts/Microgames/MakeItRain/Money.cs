8a48a14475a33d03e0565c103256ab0eedac072a
Widdiful <Widdiful@users.noreply.github.com>
2018-12-28 01:10:55 +0000

    Make it rain

---
+++ FMP/Assets/Scripts/Microgames/MakeItRain/Money.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour {
+
+    public float rotateSpeed;
+    public float moveSpeed;
+    public float maxY;
+    public Transform moneyPile;
+
+    private float timer = 0;
+
+    private void OnEnable() {
+        transform.rotation = Quaternion.identity;
+        transform.position = new Vector3(moneyPile.position.x, moneyPile.position.y + moneyPile.localScale.y + 1.0f, moneyPile.position.z);
+        maxY = moneyPile.position.y + moneyPile.localScale.y + 2.0f;
+    }
+
+    private void Update() {
+        transform.Rotate(-rotateSpeed, 0, 0);
+        transform.Translate(0, moveSpeed, 0, Space.World);
+        if (transform.position.y >= maxY) gameObject.SetActive(false);
+    }
+}
---
+++ FMP/Assets/Scripts/Microgames/MakeItRain/MoneyPile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyPile : MonoBehaviour {
+
+    public List<GameObject> moneys = new List<GameObject>();
+    public ParticleSystem particles;
+    public int requiredMoney;
+
+    float baseHeight;
+    int moneyThrown;
+    float touchStartY;
+    bool canThrow;
+
+    private void Start() {
+        baseHeight = transform.localScale.y;
+    }
+
+    private void Update() {
+        if (moneyThrown < requiredMoney && Input.touchCount > 0) {
+            if (Input.touches[0].phase == TouchPhase.Began) {
+                touchStartY = Input.touches[0].position.y / Screen.height;
+                canThrow = true;
+            }
+            if (Input.touches[0].phase == TouchPhase.Moved) {
+                float touchCurrentY = Input.touches[0].position.y / Screen.height;
+                if (Mathf.Abs(touchCurrentY - touchStartY) > 0.1f && canThrow) {
+                    ThrowMoney();
+                    canThrow = false;
+                }
+            }
+        }
+    }
+
+    private void ThrowMoney() {
+        foreach(GameObject money in moneys) {
+            if (!money.activeInHierarchy) {
+                money.SetActive(true);
+                break;
+            }
+        }
+
+        moneyThrown++;
+
+        ParticleSystem.EmissionModule emission = particles.emission;
+        emission.rateOverTime = ((float)moneyThrown / (float)requiredMoney) * 20;
+
+        transform.localScale = new Vector3(1, (1.0f - ((float)moneyThrown / (float)requiredMoney)) * baseHeight, 1);
+        if (moneyThrown == requiredMoney) {
+            GetComponentInChildren<Renderer>().enabled = false;
+            FindObjectOfType<gameManager>().CompleteGame();
+        }
+    }
+}
