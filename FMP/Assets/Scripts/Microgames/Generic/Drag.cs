5b1c9aded62e7514f640f690ff82c58f224d9800
Widdiful <Widdiful@users.noreply.github.com>
2019-02-02 04:46:03 +0000

    Started plug into mains game

---
+++ FMP/Assets/Scripts/Microgames/Generic/Drag.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    bool grabbed = false;
+    Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update () {
+        if (Input.touchCount > 0) {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[0].position), Vector2.zero);
+
+            if (hit && hit.transform == transform) {
+                grabbed = true;
+            }
+            if (grabbed) {
+                Vector3 dragPos = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+                dragPos.z = transform.position.z;
+                transform.position = dragPos;
+
+                if (rb) {
+                    rb.velocity = Vector3.zero;
+                }
+            }
+        }
+
+        else {
+            grabbed = false;
+        }
+    }
+}
---
+++ FMP/Assets/Scripts/Microgames/Generic/RopePart.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopePart : MonoBehaviour {
+
+    public bool start;
+    public int numberOfParts;
+    public Transform nextPart;
+    public bool attachEnd;
+    public Vector3 endPoint;
+
+    LineRenderer line;
+    RopePart lastPart;
+    List<Transform> parts = new List<Transform>();
+
+	void Start () {
+        line = GetComponent<LineRenderer>();
+        lastPart = this;
+        GameObject baseObject = gameObject;
+
+        if (start) {
+            for(int i = 0; i < numberOfParts; i++) {
+                GameObject newPart = Instantiate(baseObject, transform.position, transform.rotation);
+                newPart.GetComponent<RopePart>().start = false;
+                if (lastPart) {
+                    newPart.GetComponent<HingeJoint2D>().connectedBody = lastPart.GetComponent<Rigidbody2D>();
+                }
+                lastPart = newPart.GetComponent<RopePart>();
+                parts.Add(newPart.transform);
+
+                if (attachEnd && i == numberOfParts - 1) {
+                    newPart.transform.position = endPoint;
+                }
+                else {
+                    newPart.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                }
+            }
+        }
+
+        line.positionCount = parts.Count + 1;
+
+        GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+	}
+	
+	void Update () {
+		if (start) {
+            line.SetPosition(0, transform.position);
+            for(int i = 0; i < parts.Count; i++) {
+                line.SetPosition(i + 1, parts[i].position);
+            }
+
+            if (attachEnd)
+                lastPart.transform.position = endPoint;
+        }
+	}
+}
