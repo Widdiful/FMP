84583b057609df115b41dc2ba87992caa87923ad
Widdiful <Widdiful@users.noreply.github.com>
2019-02-05 00:16:26 +0000

    Finished plug into mains

---
+++ FMP/Assets/Scripts/Microgames/Generic/LockAndKey.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockAndKey : MonoBehaviour {
+
+	public enum Types { Lock, Key };
+    public Types type;
+    public bool snapToLock;
+    public static int connectionsMade;
+    public int connectionsRequired;
+
+    bool complete = false;
+    Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        LockAndKey other = collision.GetComponent<LockAndKey>();
+        if (!complete && !other.complete && type == Types.Key && other.type == Types.Lock) {
+            connectionsMade++;
+            other.complete = true;
+            complete = true;
+            if (snapToLock) {
+                transform.position = collision.transform.position;
+            }
+            if (rb) {
+                rb.gravityScale = 0;
+                rb.velocity = Vector3.zero;
+            }
+            foreach (Behaviour comp in GetComponents<Behaviour>()) {
+                if (comp != this) {
+                    comp.enabled = false;
+                }
+            }
+
+            if (connectionsMade >= connectionsRequired) {
+                gameManager.instance.CompleteGame();
+            }
+        }
+    }
+}
---
+++ FMP/Assets/Scripts/Microgames/Generic/PlaceRandomly.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceRandomly : MonoBehaviour {
+
+    public Vector3 minPos, maxPos;
+
+	void Start () {
+        transform.position = new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), Random.Range(minPos.z, maxPos.z));
+	}
+}
