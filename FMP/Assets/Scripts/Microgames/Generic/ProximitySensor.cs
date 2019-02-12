39d771465a50560092aec9db0f245d594992925d
Widdiful <Widdiful@users.noreply.github.com>
2019-02-12 04:10:45 +0000

    Got proximity sensor working, programmed waving game

---
+++ FMP/Assets/Scripts/Microgames/Generic/ProximitySensor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximitySensor : MonoBehaviour {
+
+    public static ProximitySensor instance;
+
+    public bool nearby;
+
+    void Awake() {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    void Start() {
+        PAProximity.onProximityChange += SetScaleByProximity;
+
+        SetScaleByProximity(PAProximity.proximity);
+    }
+
+    void OnDestroy() {
+        PAProximity.onProximityChange -= SetScaleByProximity;
+    }
+
+    void SetScaleByProximity(PAProximity.Proximity arg) {
+        if (arg == PAProximity.Proximity.NEAR) nearby = true;
+        else nearby = false;
+    }
+}
---
+++ FMP/Assets/Scripts/Microgames/WaveToPerson/ProximityWave.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityWave : MonoBehaviour {
+
+    public float closeRotation;
+    public float farRotation;
+    public float lerpSpeed;
+    public int wavesDone;
+    public int wavesRequired;
+
+    private bool complete;
+    private bool passedCentre;
+    private bool isLeft;
+    private enum Stages { MovingLeft, MovingRight };
+    private Stages stage = Stages.MovingLeft;
+
+    private void Update() {
+        if (ProximitySensor.instance.nearby) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), lerpSpeed);
+            if (passedCentre == false) {
+                wavesDone++;
+                if (wavesDone >= wavesRequired && !complete) {
+                    complete = true;
+                    gameManager.instance.CompleteGame();
+                }
+            }
+            passedCentre = true;
+        }
+        else {
+            if (stage == Stages.MovingLeft && passedCentre) {
+                stage = Stages.MovingRight;
+                isLeft = true;
+                passedCentre = false;
+            }
+            if (stage == Stages.MovingRight && passedCentre) {
+                stage = Stages.MovingLeft;
+                isLeft = false;
+                passedCentre = false;
+            }
+        }
+
+        if (!passedCentre) {
+            if (isLeft) {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, closeRotation), lerpSpeed);
+            }
+            else {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, farRotation), lerpSpeed);
+            }
+        }
+
+    }
+
+}
