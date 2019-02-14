4a09e5ee09b7c723c8615f8d6565dddd39495586
Widdiful <Widdiful@users.noreply.github.com>
2019-02-14 00:19:34 +0000

    Launch person game

---
+++ FMP/Assets/Scripts/Microgames/InflateBalloon/InflateBalloon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InflateBalloon : MonoBehaviour {
+
+    public float lerpSpeed;
+    public float maxSize;
+    bool pumping;
+
+    private void Update() {
+        if (!pumping && ProximitySensor.instance.nearby) {
+            pumping = true;
+            transform.localScale = Vector3.Lerp(transform.localScale, transform.localScale * maxSize, lerpSpeed);
+        }
+        else if (!ProximitySensor.instance.nearby) {
+            pumping = false;
+        }
+    }
+}
