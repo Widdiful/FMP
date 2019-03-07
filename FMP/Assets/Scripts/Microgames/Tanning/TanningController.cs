4bc9682be6983ed4487b8756f3391e7fdd34e9a4
Widdiful <Widdiful@users.noreply.github.com>
2019-03-07 03:34:07 +0000

    Tanning game

---
+++ FMP/Assets/Scripts/Microgames/Tanning/TanningController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TanningController : MonoBehaviour {
+
+    public float tanPercentage;
+    public float increasePerSecond;
+    public float decreasePerSecond;
+    public Gradient skinTone;
+
+    public Slider slider;
+    public SpriteRenderer character;
+    public Timer timer;
+
+	void Update () {
+        if (!ProximitySensor.instance.nearby)
+            tanPercentage += increasePerSecond * Time.deltaTime;
+        else
+            tanPercentage -= decreasePerSecond * Time.deltaTime;
+
+        tanPercentage = Mathf.Clamp01(tanPercentage);
+
+        if (tanPercentage >= 0.4f && tanPercentage <= 0.6f)
+            timer.winOnTimeOver = true;
+        else
+            timer.winOnTimeOver = false;
+    }
+
+    private void FixedUpdate() {
+        slider.value = tanPercentage;
+        character.color = skinTone.Evaluate(tanPercentage);
+    }
+}
