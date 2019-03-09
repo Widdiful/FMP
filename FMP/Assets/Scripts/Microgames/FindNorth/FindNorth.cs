86603964e89387a32c2c707eb6cc59f935bc18cb
Widdiful <Widdiful@users.noreply.github.com>
2019-03-09 05:25:12 +0000

    Scare game

---
+++ FMP/Assets/Scripts/Microgames/FindNorth/FindNorth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindNorth : MonoBehaviour {
+
+    private void Start() {
+        //Input.compass.enabled = true;
+        //Input.location.Start();
+    }
+
+    void Update () {
+        transform.rotation = Quaternion.Euler(0, 0, -Input.compass.magneticHeading);
+    }
+}
--- FMP/Assets/Scripts/Microgames/Generic/AccelerometerCamera.cs
+++ FMP/Assets/Scripts/Microgames/Generic/AccelerometerCamera.cs
@@ -5,8 +5,38 @@ using UnityEngine;
 public class AccelerometerCamera : MonoBehaviour {
 
     public float speed;
+    public Transform anchor;
+    public Transform characterAnchor;
+    public Transform character;
+    Quaternion rot;
+    bool scared;
 
-	void Update () {
-        transform.Translate(Input.acceleration.x * Time.deltaTime, 0, 0);
+    private void Start() {
+        rot = new Quaternion(0, 0, 1, 0);
+
+        Input.gyro.enabled = false;
+        Input.gyro.enabled = true;
+    }
+
+    void Update () {
+        transform.localRotation = Input.gyro.attitude * rot;
+
+        if (!scared) {
+            characterAnchor.Rotate(0, 0, speed * Time.deltaTime);
+
+            if (MicManager.instance.levelMax >= 0.75f) {
+                RaycastHit hit;
+                if (Physics.SphereCast(transform.position, 1.0f, transform.forward, out hit, 10)) {
+                    if (hit.transform.CompareTag("Enemy")) {
+                        scared = true;
+                        character = hit.transform;
+                        gameManager.instance.CompleteGame();
+                    }
+                }
+            }
+        }
+        else {
+            character.Translate(-speed * Time.deltaTime, 0, 0, Space.Self);
+        }
 	}
 }
