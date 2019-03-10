6f842ad3928d468f95a229960699d8a815fbd655
Widdiful <Widdiful@users.noreply.github.com>
2019-03-10 07:23:38 +0000

    Inflate balloon game

--- FMP/Assets/Scripts/Microgames/InflateBalloon/InflateBalloon.cs
+++ FMP/Assets/Scripts/Microgames/InflateBalloon/InflateBalloon.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class InflateBalloon : MonoBehaviour {
 
     public float lerpSpeed;
-    public float maxSize;
+    public float sizeIncrease;
+    public int numberOfPumps;
+    Vector3 nextScale = Vector3.one;
+    float nextAngle;
+    Vector3 nextGoal;
+    int pumps;
     bool pumping;
+    bool completed;
 
     private void Update() {
-        if (!pumping && ProximitySensor.instance.nearby) {
+        if (!pumping && ProximitySensor.instance.nearby && !completed) {
             pumping = true;
-            transform.localScale = Vector3.Lerp(transform.localScale, transform.localScale * maxSize, lerpSpeed);
+            pumps++;
+
+            if (pumps >= numberOfPumps) {
+                completed = true;
+                gameManager.instance.CompleteGame();
+            }
+
+            else {
+                nextScale = transform.localScale * sizeIncrease;
+                nextGoal = transform.localScale + ((nextScale - transform.localScale) / 2);
+                nextAngle += (-90 / (numberOfPumps - 1));
+            }
         }
-        else if (!ProximitySensor.instance.nearby) {
+        else if (!ProximitySensor.instance.nearby && transform.localScale.x >= nextGoal.x) {
             pumping = false;
         }
+
+        transform.localScale = Vector3.Lerp(transform.localScale, nextScale, lerpSpeed * Time.deltaTime);
+        Quaternion angle = Quaternion.Euler(nextAngle, 0, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, angle, lerpSpeed * Time.deltaTime);
     }
 }
