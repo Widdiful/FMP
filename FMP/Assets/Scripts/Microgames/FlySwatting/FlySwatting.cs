55cb7ec58e720821d139cc4edf3c2fc812511ba8
Widdiful <Widdiful@users.noreply.github.com>
2019-03-16 01:41:03 +0000

    Fly swatting game

---
+++ FMP/Assets/Scripts/Microgames/FlySwatting/FlySwatting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlySwatting : MonoBehaviour {
+
+    public float speed;
+    public float speedAdjust;
+    public float wanderSize;
+    public float maxSpeed;
+    Vector2 direction;
+
+    private void Start() {
+        direction += new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        direction = direction.normalized;
+    }
+
+    void Update () {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        direction += new Vector2(Random.Range(-wanderSize, wanderSize), Random.Range(-wanderSize, wanderSize));
+        direction = direction.normalized;
+
+        speed += Random.Range(-speedAdjust, speedAdjust);
+        speed = Mathf.Clamp(speed, 0, maxSpeed);
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y * 0.01f);
+
+        if (Input.touchCount > 0) {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[0].position), Vector2.zero);
+
+            if (hit && hit.transform == transform) {
+                if (FindObjectsOfType<FlySwatting>().Length <= 1) {
+                    gameManager.instance.CompleteGame();
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        direction = Vector2.Reflect(direction, collision.contacts[0].normal);
+    }
+}
