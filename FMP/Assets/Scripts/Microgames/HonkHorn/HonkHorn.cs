05d292c21a7457bd55ddaa535688e916a7b48301
Widdiful <Widdiful@users.noreply.github.com>
2019-03-18 02:31:32 +0000

    Rock paper scissors game

---
+++ FMP/Assets/Scripts/Microgames/HonkHorn/HonkHorn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HonkHorn : MonoBehaviour {
+
+    float baseY;
+    float distance;
+    Vector3 pos1, pos2;
+
+    private void Start() {
+        baseY = transform.localScale.y;
+    }
+
+    private void Update() {
+        if (Input.touchCount > 1) {
+            pos1 = Input.GetTouch(0).position;
+            pos2 = Input.GetTouch(1).position;
+            pos1.z = 10;
+            pos2.z = 10;
+            pos1 = Camera.main.ScreenToWorldPoint(pos1);
+            pos2 = Camera.main.ScreenToWorldPoint(pos2);
+
+            var worldToLocalMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one).inverse;
+            pos1 = worldToLocalMatrix.MultiplyPoint3x4(pos1);
+            pos2 = worldToLocalMatrix.MultiplyPoint3x4(pos2);
+
+            distance = Mathf.Abs(pos1.y - pos2.y + 0.5f);
+
+            if (distance < baseY) {
+                Vector3 localScale = transform.localScale;
+                localScale.y = distance;
+                transform.localScale = localScale;
+            }
+        }
+    }
+}
---
+++ FMP/Assets/Scripts/Microgames/RockPaperScissors/RockPaperScissors.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RockPaperScissors : MonoBehaviour {
+
+	public enum HandTypes { Rock, Paper, Scissors };
+    public HandTypes enemyType;
+    public Text text;
+
+    private void Start() {
+        enemyType = (HandTypes)Random.Range(0, 3);
+        text.text = enemyType.ToString();
+    }
+
+    public void PlayRock() {
+        if (enemyType == HandTypes.Scissors) {
+            GetComponent<Canvas>().enabled = false;
+            gameManager.instance.CompleteGame();
+        }
+        else {
+            StartCoroutine(FailGame());
+        }
+    }
+
+    public void PlayPaper() {
+        if (enemyType == HandTypes.Rock) {
+            GetComponent<Canvas>().enabled = false;
+            gameManager.instance.CompleteGame();
+        }
+        else {
+            StartCoroutine(FailGame());
+        }
+    }
+
+    public void PlayScissors() {
+        if (enemyType == HandTypes.Paper) {
+            GetComponent<Canvas>().enabled = false;
+            gameManager.instance.CompleteGame();
+        }
+        else {
+            StartCoroutine(FailGame());
+        }
+    }
+
+    IEnumerator FailGame() {
+        GetComponent<Canvas>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        gameManager.instance.FailGame();
+    }
+}
