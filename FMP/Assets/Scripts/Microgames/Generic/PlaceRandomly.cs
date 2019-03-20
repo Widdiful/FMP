1651c643781ccc43b6f4051443377447870a64d0
Widdiful <Widdiful@users.noreply.github.com>
2019-03-20 01:44:30 +0000

    Unlock chest game

--- FMP/Assets/Scripts/Microgames/Generic/PlaceRandomly.cs
+++ FMP/Assets/Scripts/Microgames/Generic/PlaceRandomly.cs
@@ -8,13 +8,24 @@ public class PlaceRandomly : MonoBehaviour {
     public bool x = true;
     public bool y = true;
     public bool z = true;
+    public bool localPosition;
 
 	void Awake () {
-        if (x)
-            transform.position = new Vector3(Random.Range(minPos.x, maxPos.x), transform.position.y, transform.position.z);
-        if (y)
-            transform.position = new Vector3(transform.position.x, Random.Range(minPos.y, maxPos.y), transform.position.z);
-        if (z)
-            transform.position = new Vector3(transform.position.x, transform.position.y, Random.Range(minPos.z, maxPos.z));
+        if (!localPosition) {
+            if (x)
+                transform.position = new Vector3(Random.Range(minPos.x, maxPos.x), transform.position.y, transform.position.z);
+            if (y)
+                transform.position = new Vector3(transform.position.x, Random.Range(minPos.y, maxPos.y), transform.position.z);
+            if (z)
+                transform.position = new Vector3(transform.position.x, transform.position.y, Random.Range(minPos.z, maxPos.z));
+        }
+        else {
+            if (x)
+                transform.localPosition = new Vector3(Random.Range(minPos.x, maxPos.x), transform.localPosition.y, transform.localPosition.z);
+            if (y)
+                transform.localPosition = new Vector3(transform.localPosition.x, Random.Range(minPos.y, maxPos.y), transform.localPosition.z);
+            if (z)
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Random.Range(minPos.z, maxPos.z));
+        }
 	}
 }
---
+++ FMP/Assets/Scripts/Microgames/UnlockChest/ChestKey.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestKey : MonoBehaviour {
+
+    public Vector3 pickupOffset;
+    public GameObject particles;
+    Transform target;
+    bool complete;
+    bool pickedUp;
+
+    private void LateUpdate() {
+        if (target) {
+            transform.position = target.position + pickupOffset;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player") && !pickedUp) {
+            transform.parent = null;
+            target = collision.transform;
+            transform.GetComponent<CircleCollider2D>().enabled = false;
+            transform.GetComponent<BoxCollider2D>().enabled = true;
+            pickedUp = true;
+        }
+        else if (collision.name == "Chest" && !complete) {
+            complete = true;
+            HideKey();
+            particles.SetActive(true);
+            gameManager.instance.CompleteGame();
+        }
+    }
+
+    public void HideKey() {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
