a19c01aad41dbb46014c75ac05e3afdab50741f0
Widdiful <Widdiful@users.noreply.github.com>
2019-03-21 02:41:44 +0000

    Type on keyboard

---
+++ FMP/Assets/Scripts/Microgames/PickUpChopsticks/PickUpChopsticks.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpChopsticks : MonoBehaviour {
+
+    public bool grabbed;
+    public bool doubleGrabbed;
+    float gravityScale;
+    Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        print(Input.touchCount);
+        if (Input.touchCount > 0) {
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[0].position), Vector2.zero);
+            if (hit && hit.transform == rb.transform) {
+                if (!grabbed) {
+                    grabbed = true;
+                    doubleGrabbed = false;
+                }
+            }
+
+            if (Input.touchCount > 1) {
+                hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[1].position), Vector2.zero);
+                if (hit && hit.transform == rb.transform) {
+                    if (!doubleGrabbed) {
+                        grabbed = false;
+                        doubleGrabbed = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        if (Input.touchCount == 1 && grabbed) {
+            Vector3 dragPos = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+            dragPos.y = transform.position.y;
+            dragPos.z = transform.position.z;
+            rb.velocity = (dragPos - transform.position) * Time.deltaTime;
+            transform.position = dragPos;
+        }
+        else if (Input.touchCount > 1 && doubleGrabbed) {
+            Vector3 dragPos = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+            dragPos.z = transform.position.z;
+            rb.velocity = (dragPos - transform.position) * Time.deltaTime;
+            transform.position = dragPos;
+        }
+    }
+}
---
+++ FMP/Assets/Scripts/Microgames/TypeOnKeyboard/TypeOnKeyboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeOnKeyboard : MonoBehaviour {
+
+    public List<string> messages = new List<string>();
+    public Text text;
+    int lettersTyped;
+    string message;
+    bool complete;
+
+    private void Start() {
+        message = messages[Random.Range(0, messages.Count)];
+    }
+
+    public void Type() {
+        if (!complete) {
+            if (text.text.Length < message.Length) {
+                text.text += message[lettersTyped];
+                lettersTyped++;
+            }
+            else {
+                complete = true;
+                gameManager.instance.CompleteGame();
+            }
+        }
+    }
+}
